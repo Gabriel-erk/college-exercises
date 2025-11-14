5c57e4abdea7d81776cfd4c25a7b4d8561aac235
Gabriel Erick <gabrielmmve13@gmail.com>
2025-11-14 18:11:55 -0300

    alterações

--- back-end-fluency-academy/controllers/clienteController.js
+++ back-end-fluency-academy/controllers/clienteController.js
@@ -24,4 +24,35 @@ module.exports = class ClienteController {
         });
       });
   }
+
+//   static async inserir(req, res) {
+//     console.log(req.body);
+
+//     const pessoa = new Pessoa({
+//       nome: req.body.nome,
+//       cpf: req.body.cpf,
+//       idade: req.body.idade,
+//       dtNascimento: req.body.dtNascimento,
+//       telefone: req.body.telefone,
+//       email: req.body.email,
+//       nuCarteiraMotorista: req.body.nuCarteiraMotorista,
+//       anoVencimentoCarteira: req.body.anoVencimentoCarteira,
+//     });
+
+//     pessoa.save(pessoa).then((data) => {
+//       const cliente = new Cliente({
+//         idPessoa: data._id,
+//         nuCarteiraMotorista: req.body.nuCarteiraMotorista,
+//         anoVencimentoCarteira: req.body.anoVencimentoCarteira,
+//       });
+
+//       cliente.save(cliente).then(data => {
+//         res.send(data);
+//       }).catch(err => {
+//         res.status(500).send({
+//           message: err.message || "Ocorreu um erro ao inserir o cliente.",
+//         });
+//       });
+//     });
+//   }
 };
--- back-end-fluency-academy/controllers/esportivoController.js
+++ back-end-fluency-academy/controllers/esportivoController.js
@@ -1,5 +1,26 @@
 const esportivo = require("./models/promocao");
 
 module.exports = class EsportivoController {
- 
+  static async inserir(req, res) {
+    console.log(req.body);
+    const carro = await Carro.findOne({ placa: req.body.placa });
+
+    const esportivo = new Esportivo({
+      idCarro: carro._id,
+      tp100km: req.body.tp100km,
+      melhorias: req.body.melhorias,
+    });
+
+    esportivo
+      .save(esportivo)
+      .then((data) => {
+        res.send(data);
+      })
+      .catch((err) => {
+        res.status(500).send({
+          message:
+            err.message || `Ocorreu um erro ao inserir o carro esportivo: ${esportivo}.`,
+        });
+      });
+  }
 };
