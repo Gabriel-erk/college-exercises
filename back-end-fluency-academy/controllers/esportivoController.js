3cc3d88b2cc7394c68d1100910d8e60fe95bf5ce
Gabriel Erick <gabrielmmve13@gmail.com>
2025-11-29 13:56:05 -0300

    esportivo ajustes

--- back-end-fluency-academy/controllers/esportivoController.js
+++ back-end-fluency-academy/controllers/esportivoController.js
@@ -1,26 +1,34 @@
-const esportivo = require("../models/promocao");
+const Esportivo = require("../models/esportivo");
+const Carro = require("../models/Carro");
 
 module.exports = class EsportivoController {
   static async inserir(req, res) {
-    console.log(req.body);
-    const carro = await Carro.findOne({ placa: req.body.placa });
+    try {
+      console.log(req.body);
 
-    const esportivo = new Esportivo({
-      idCarro: carro._id,
-      tp100km: req.body.tp100km,
-      melhorias: req.body.melhorias,
-    });
+      // achar o carro pela placa
+      const carro = await Carro.findOne({ placa: req.body.placa });
 
-    esportivo
-      .save(esportivo)
-      .then((data) => {
-        res.send(data);
-      })
-      .catch((err) => {
-        res.status(500).send({
-          message:
-            err.message || `Ocorreu um erro ao inserir o carro esportivo: ${esportivo}.`,
-        });
+      if (!carro) {
+        return res.status(404).send({ message: "Carro não encontrado." });
+      }
+
+      // criar objeto esportivo corretamente
+      const novoEsportivo = new Esportivo({
+        idCarro: carro._id,
+        tp100km: req.body.tp100km,
+        melhorias: req.body.melhorias,
       });
+
+      const data = await novoEsportivo.save();
+
+      res.send(data);
+    } catch (err) {
+      res.status(500).send({
+        message:
+          err.message ||
+          "Ocorreu um erro ao inserir o carro esportivo.",
+      });
+    }
   }
 };
--- back-end-fluency-academy/models/esportivo.js
+++ back-end-fluency-academy/models/esportivo.js
@@ -1,17 +1,16 @@
 const mongoose = require("mongoose");
-const Esportivo = mongoose.Schema;
-({
-  idCarro: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "Carro",
-    required: true,
-  },
-  tp100km: { type: Number, required: true },
-  melhorias: {
-    type: [String],
-    required: true,
-  }
-}),
-  { versionKey: false };
 
-module.exports = mongoose.model("Esportivo", Esportivo);
+const EsportivoSchema = new mongoose.Schema(
+  {
+    idCarro: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Carro",
+      required: true,
+    },
+    tp100km: { type: Number, required: true },
+    melhorias: { type: [String], required: true },
+  },
+  { versionKey: false }
+);
+
+module.exports = mongoose.model("Esportivo", EsportivoSchema);
